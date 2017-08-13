e548b115ae788a87e285862bba5f27d5f744a9d8
TheOneHyer <theonehyer@gmail.com>
2017-08-13 16:35:35 -0600

    Added basic setup.py
    Added a basic setup.py that will need additional work later

---
+++ setup.py
@@ -0,0 +1,62 @@
+#! /usr/bin/env python
+
+"""Setup file to build and install oak
+
+Copyright:
+
+    setup.py build and install oak
+    Copyright (C) 2017  Alex Hyer
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from setuptools import setup
+
+__author__ = 'Alex Hyer'
+__email__ = 'theonehyer@gmail.com'
+__license__ = 'GPLv3'
+__maintainer__ = 'Alex Hyer'
+__status__ = 'Alpha'
+__version__ = '0.1.0a1'
+
+setup(name='oak',
+      version='0.1.0a1',
+      description='Ontological Annotation using Kegg',
+      classifiers=[
+          'Development Status :: 1 - Pre-production',  # Edit this?
+          'Intended Audience :: End Users/Desktop',
+          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+          'Natural Language :: English',
+          'Operating System :: OS Independent', # Maybe?
+          'Programming Language :: Python :: 3.5',
+          'Topic :: Security'  # Change this
+      ],
+      keywords='secure password generator',
+      url='https://github.com/TheOneHyer/aspgen',
+      download_url='https://github.com/Brazelton-Lab/oak/tarball/0.1.0a1',
+      author='Alex Hyer',
+      author_email='theonehyer@gmail.com',
+      license='GPLv3',
+      packages=[
+          # Add packages
+      ],
+      include_package_data=True,
+      zip_safe=False,
+      entry_points={
+          # Add entry points
+      },
+      requires=[
+          # Add entry points
+      ]
+      )
